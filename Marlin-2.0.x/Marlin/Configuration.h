4be7f1c440177a5dfc1767f8075807212a87075e
Olav Gausaker <olav@gausaker.com>
2021-05-26 15:04:34 +0200

    For Zdeněk Matěj

--- Marlin-2.0.x/Marlin/Configuration.h
+++ Marlin-2.0.x/Marlin/Configuration.h
@@ -740,7 +740,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 160, 160, 800, 185 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 800, 185 }
 
 /**
  * Default Max Feed Rate (mm/s)
@@ -1085,9 +1085,9 @@
 // @section machine
 
 // Invert the stepper direction. Change (or reverse the motor connector) if an axis goes the wrong way.
-#define INVERT_X_DIR false
-#define INVERT_Y_DIR false
-#define INVERT_Z_DIR true
+#define INVERT_X_DIR true
+#define INVERT_Y_DIR true
+#define INVERT_Z_DIR false
 
 // @section extruder
 
